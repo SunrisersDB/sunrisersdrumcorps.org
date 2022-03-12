b23e8b4a46d0e8e6339848e344463ac2172ee053
Lee <plbrannan95@gmail.com>
2022-03-12 16:02:17 -0500

    Add nextjs OAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import "../styles/globals.scss";
 // import { config } from "@fortawesome/fontawesome-svg-core";
 // config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
-export default MyApp;
+export default App;
