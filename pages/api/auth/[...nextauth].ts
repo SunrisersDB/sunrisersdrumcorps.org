303774dac04cb8bda001b0c98013068046f86e15
Lee <plbrannan95@gmail.com>
2022-03-15 10:47:19 -0400

    Login with magic link

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,20 +1,39 @@
-import { NextApiHandler } from 'next';
-import NextAuth from 'next-auth';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import GitHubProvider from 'next-auth/providers/github';
-import { PrismaClient } from '@prisma/client';
-import prisma from '../../../lib/prisma';
+import { NextApiHandler } from "next";
+import NextAuth, { NextAuthOptions, Theme } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GitHubProvider from "next-auth/providers/github";
+import prisma from "../../../lib/prisma";
+import EmailProvider from "next-auth/providers/email";
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
 
-const options = {
+const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+
+    EmailProvider({
+      name: "Magic Link",
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      // FIXME -- Doesn't work, all mail comes from hi@bananabrann.dev
+      // from: process.env.EMAIL_FROM,
+    }),
   ],
   adapter: PrismaAdapter(prisma),
-  secret: process.env.NEXTAUTH_SECRET
+  secret: process.env.NEXTAUTH_SECRET,
+  theme: {
+    colorScheme: "light",
+    brandColor: "#F87C39",
+    logo: "https://sunrisers.blob.core.windows.net/web-assets/logo-text.svg",
+  },
 };
